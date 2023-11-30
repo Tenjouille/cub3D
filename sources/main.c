a72aa09c7fa8c737f9fcdf3bb37203c0b86ec8f7
Thomas GEORGE <tgeorge@made-f0Br5s11.clusters.42paris.fr>
2023-11-30 19:38:48 +0100

    remove memory_manager

--- sources/main.c
+++ sources/main.c
@@ -29,32 +29,6 @@ int	ft_argv_parsing(int ac, char **av)
 	return (0);
 }
 
-char	**ft_get_map(t_cub *cub, char **desc)
-{
-	char	**map;
-	int		i;
-	int		j;
-	int		k;
-
-	i = 0;
-	j = 0;
-	k = 0;
-	while (desc[i])
-		i++;
-	j = i - 1;
-	while (!ft_strchr(desc[j], '.') && !ft_strchr(desc[j], ','))
-		j--;
-	j++;
-	map = malloc(sizeof(char*) * (i - j + 1));
-	if (!map)
-		return (ft_putstr_fd("malloc FAILURE\n", 2), NULL);
-	while (desc[j] && j < i)
-		map[k++] = ft_strdup(desc[j++]);
-	map[k] = 0;
-	cub->map_height = k - 1;
-	return (map);
-}
-
 int	main(int ac, char **av)
 {
 	t_cub	cub;
